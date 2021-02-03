3dcbc421ebaa527391eb76c169b5adca8498b570
Tae.Garam <drasla@naver.com>
2021-02-03 23:49:36 +0900

    #3.4 Location Aware header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -18,15 +18,14 @@ box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 
 const List = styled.ul`
 display: flex;
-&:hover {
-    background-color: blue;
-}
 `;
 
 const Item = styled.li`
 width: 80px;
 height: 50px;
 text-align: center;
+  border-bottom: 5px solid ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom .5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -36,12 +35,12 @@ align-items: center;
 justify-content: center;
 `;
 
-export default () => (
+export default withRouter(({location: { pathname }}) => (
     <Header>
         <List>
-            <Item><SLink to="/">Movies</SLink></Item>
-            <Item><SLink to="/tv">TV</SLink></Item>
-            <Item><SLink to="/search">Search</SLink></Item>
+            <Item current={pathname === "/"}><SLink to="/">Movies</SLink></Item>
+            <Item current={pathname === "/tv"}><SLink to="/tv">TV</SLink></Item>
+            <Item current={pathname === "/search"}><SLink to="/search">Search</SLink></Item>
         </List>
     </Header>
-)
+));
