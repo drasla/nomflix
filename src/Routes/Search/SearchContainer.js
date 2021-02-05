58a8073344fced5b962df626ab0c51ae0e455e61
Tae.Garam <drasla@naver.com>
2021-02-05 18:20:50 +0900

    #5.4 Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import {moviesApi, tvApi} from "../../api";
 
 export default class extends React.Component{
     state = {
@@ -10,10 +11,32 @@ export default class extends React.Component{
         loading: false
     };
 
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if(searchTerm !== "") {
+            this.searchByTerm();
+        }
+    }
+
+    searchByTerm = async () => {
+        const { searchTerm } = this.state;
+        this.setState({ loading: true });
+        try {
+            const {data: {results: movieResults}} = await moviesApi.search(searchTerm);
+            const {data: {results: tvResults}} = await tvApi.search(searchTerm);
+            this.setState({ movieResults, tvResults });
+        } catch {
+            this.setState({ error: "Can't find results."})
+        } finally {
+            this.setState({ loading: false });
+        }
+    }
+
     render() {
-        const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+        const {movieResults, tvResults, searchTerm, error, loading} = this.state;
         return (
-            <SearchPresenter movieResults={movieResults} tvResults={tvResults} searchTerm={searchTerm} error={error} loading={loading}/>
+            <SearchPresenter movieResults={movieResults} tvResults={tvResults} searchTerm={searchTerm} error={error}
+                             loading={loading} handleSubmit={this.handleSubmit} />
         );
     };
 }
