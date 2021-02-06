ac324ba11f9f951a547679bc0967bd68791cfa29
Tae.Garam <drasla@naver.com>
2021-02-07 00:01:33 +0900

    #6.7 Poster Component part Two

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -1,28 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  background-image: url(${props => props.bgUrl});
+  background-size: cover;
+  border-radius: 4px;
+  background-position: center center;
+  height: 180px;
+  transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+  bottom: 5px;
+  right: 5px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
 
-const Title = styled.span``;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
 
-const Year = styled.span``;
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
 
-const Poster = ({ id, imageUrl, title, rating, year, isMovie=false }) => (
-    <Link to={isMovie? `/movie/${id}` : `/show/${id}`}>
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
+
+const Poster = ({id, imageUrl, title, rating, year, isMovie = false}) => (
+    <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
         <Container>
             <ImageContainer>
-                <Image bgUrl={imageUrl} />
+                <Image bgUrl={imageUrl ? `https://image.tmdb.org/t/p/w300/${imageUrl}` : require("../assets/noPosterSmall.png").default}/>
                 <Rating><span role="img" aria-label="rating">🌟</span>{" "}{rating}/10</Rating>
             </ImageContainer>
-            <Title>{title}</Title>
+            <Title>{title.length > 18 ? `${title.substring(0,18)}...` : title}</Title>
             <Year>{year}</Year>
         </Container>
     </Link>
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Loader from "../../Components/Loader";
 import Section from "../../Components/Section";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div``;
 
@@ -25,11 +26,18 @@ const SearchPresenter = ({movieResults, tvResults, searchTerm, handleSubmit, upd
         </Form>
         {loading ? <Loader/> : <>
             {movieResults && movieResults.length > 0 &&
-            <Section title="Movie Results">{movieResults.map(movie => <span
-                key={movie.id}>{movie.title}</span>)}</Section>}
+            <Section title="Movie Results">{movieResults.map(movie => <Poster key={movie.id} id={movie.id}
+                                                                          title={movie.original_title}
+                                                                          imageUrl={movie.poster_path}
+                                                                          rating={movie.vote_average}
+                                                                          year={movie.release_date && movie.release_date.substring(0, 4)}
+                                                                          isMovie={true}/>)}</Section>}
             {tvResults && tvResults.length > 0 &&
-            <Section title="TV Results">{tvResults.map(show => <span
-                key={show.id}>{show.name}</span>)}</Section>}
+            <Section title="TV Results">{tvResults.map(show => <Poster key={show.id} id={show.id}
+                                                                        title={show.original_name}
+                                                                        imageUrl={show.poster_path}
+                                                                        rating={show.vote_average}
+                                                                        year={show.first_air_date && show.first_air_date.substring(0, 4)}/>)}</Section>}
         </>}
         {error && <Message text={error} color="#e74c3c"/>}
         {tvResults && movieResults && tvResults.length === 0 && movieResults.length === 0 &&
