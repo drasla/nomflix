403b8b60f2e4e24367df02a75a0ce57f7420618c
Tae.Garam <drasla@naver.com>
2021-02-06 19:37:13 +0900

    #6.2 TVPresenter and Loader Components

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,21 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
-const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) => loading ? null :
+const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) => loading ? <Loader /> :
     <Container>
         {nowPlaying && nowPlaying.length > 0 && (
-            <Section title="Now Playing">{nowPlaying.map(movie => movie.title)}</Section>
+            <Section title="Now Playing">{nowPlaying.map(movie => <span>{movie.title}</span>)}</Section>
         )}
         {upcoming && upcoming.length > 0 && (
-            <Section title="Upcoming">{upcoming.map(movie => movie.title)}</Section>
+            <Section title="Upcoming">{upcoming.map(movie => <span>{movie.title}</span>)}</Section>
         )}
         {popular && popular.length > 0 && (
-            <Section title="Popular">{popular.map(movie => movie.title)}</Section>
+            <Section title="Popular">{popular.map(movie => <span>{movie.title}</span>)}</Section>
         )}
     </Container>;
 
