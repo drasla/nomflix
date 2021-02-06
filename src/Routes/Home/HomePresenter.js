5907824ac350ed22d4e57e39d81ea097b4d94b00
Tae.Garam <drasla@naver.com>
2021-02-06 23:17:07 +0900

    #6.6 Rendering Poster Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -13,13 +13,28 @@ const Container = styled.div`
 const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) => loading ? <Loader /> :
     <Container>
         {nowPlaying && nowPlaying.length > 0 && (
-            <Section title="Now Playing">{nowPlaying.map(movie => <Poster />)}</Section>
+            <Section title="Now Playing">{nowPlaying.map(movie => <Poster key={movie.id} id={movie.id}
+                                                                          title={movie.original_title}
+                                                                          imageUrl={movie.poster_path}
+                                                                          rating={movie.vote_average}
+                                                                          year={movie.release_date && movie.release_date.substring(0, 4)}
+                                                                          isMovie={true}/>)}</Section>
         )}
         {upcoming && upcoming.length > 0 && (
-            <Section title="Upcoming">{upcoming.map(movie => <Poster />)}</Section>
+            <Section title="Upcoming">{upcoming.map(movie => <Poster key={movie.id} id={movie.id}
+                                                                     title={movie.original_title}
+                                                                     imageUrl={movie.poster_path}
+                                                                     rating={movie.vote_average}
+                                                                     year={movie.release_date && movie.release_date.substring(0, 4)}
+                                                                     isMovie={true}/>)}</Section>
         )}
         {popular && popular.length > 0 && (
-            <Section title="Popular">{popular.map(movie => <Poster />)}</Section>
+            <Section title="Popular">{popular.map(movie => <Poster key={movie.id} id={movie.id}
+                                                                   title={movie.original_title}
+                                                                   imageUrl={movie.poster_path}
+                                                                   rating={movie.vote_average}
+                                                                   year={movie.release_date && movie.release_date.substring(0, 4)}
+                                                                   isMovie={true}/>)}</Section>
         )}
         {error && <Message text={error} color="#e74c3c" />}
     </Container>;
